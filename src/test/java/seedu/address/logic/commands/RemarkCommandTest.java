d8bd73557ea0eea397698f090e88cfe6535f4e1d
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-02-28 21:55:28 +0800

    Add a placeholder element for remark to the UI

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,10 +1,16 @@
 package seedu.address.logic.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class RemarkCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     @Test
     public void execute() {
         assertCommandFailure(new RemarkCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
