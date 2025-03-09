5cc46eb2c4e391e2f37ff581d08754a7ae894cc2
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-09 18:56:14 +0800

    Add more test cases for PriorityCommand

--- src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FOURTH_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -40,10 +44,43 @@ public class PriorityCommandTest {
     }
 
     @Test
+    public void execute_toggleOffPriorityUnfilteredList_success() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FOURTH_PERSON.getZeroBased());
+        Person priorityPerson = new PersonBuilder(firstPerson).withTags(VALID_TAG_FRIEND).build();
+
+        PriorityCommand priorityCommand = new PriorityCommand(INDEX_FOURTH_PERSON);
+
+        String expectedMessage = String.format(PriorityCommand.MESSAGE_PRIORITY_PERSON_SUCCESS,
+                Messages.format(priorityPerson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(firstPerson, priorityPerson);
+
+        assertCommandSuccess(priorityCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
     public void execute_invalidIndex_throwsCommandException() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
         PriorityCommand priorityCommand = new PriorityCommand(outOfBoundIndex);
 
         assertCommandFailure(priorityCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        final PriorityCommand standardCommand = new PriorityCommand(INDEX_FIRST_PERSON);
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new PriorityCommand(INDEX_SECOND_PERSON)));
     }
 }
