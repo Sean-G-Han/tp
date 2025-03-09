8b67e5d9c59d981a2bbd3036d50a39fb60a844e9
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-09 18:38:55 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
@@ -12,13 +11,11 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 
 public class PriorityCommandTest {
