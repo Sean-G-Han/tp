289d929021ff382dbe3d7140431c3b2a7f134b87
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-09 18:34:26 +0800

    Add extra test cases for CoderCov

--- src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PriorityCommandTest.java
@@ -1,19 +1,24 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 
 public class PriorityCommandTest {
@@ -35,6 +40,13 @@ public class PriorityCommandTest {
         expectedModel.setPerson(firstPerson, priorityPerson);
 
         assertCommandSuccess(priorityCommand, model, expectedMessage, expectedModel);
+    }
 
+    @Test
+    public void execute_invalidIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        PriorityCommand priorityCommand = new PriorityCommand(outOfBoundIndex);
+
+        assertCommandFailure(priorityCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 }
