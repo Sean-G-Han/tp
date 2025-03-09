71bea969a77195df89966605740e8b48bf70a374
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-09 16:38:18 +0800

    Implement priority command

--- src/main/java/seedu/address/logic/commands/PriorityCommand.java
+++ src/main/java/seedu/address/logic/commands/PriorityCommand.java
@@ -1,10 +1,22 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 
 /**
  * Marks a client as a priority
@@ -12,6 +24,8 @@ import seedu.address.model.Model;
 public class PriorityCommand extends Command {
 
     public static final String COMMAND_WORD = "priority";
+
+    public static final String MESSAGE_PRIORITY_PERSON_SUCCESS = "Prioritised Person: %1$s";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Marks the person identified by the index number used in the displayed person list as a priority.\n"
@@ -22,19 +36,56 @@ public class PriorityCommand extends Command {
 
     private final Index index;
 
+    private Set<Tag> tags;
+
     /**
      * @param index of the person in the filtered person list to mark as priority
      */
     public PriorityCommand(Index index) {
         requireNonNull(index);
-
         this.index = index;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(
-                String.format(MESSAGE_ARGUMENTS, index.getOneBased()));
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = addPriorityTag(personToEdit);
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_PRIORITY_PERSON_SUCCESS, Messages.format(editedPerson)));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * With a new tag called priority
+     */
+    private static Person addPriorityTag(Person personToEdit) throws CommandException {
+        assert personToEdit != null;
+
+        // Create a mutable set
+        Set<Tag> tags = new HashSet<>(personToEdit.getTags());
+
+        if (tags.stream().anyMatch(tag -> "Priority".equals(tag.tagName))) {
+            throw new CommandException(Messages.MESSAGE_PERSON_ALREADY_PRIORITY);
+        }
+
+        tags.add(new Tag("Priority"));
+
+        return new Person(
+                personToEdit.getName(),
+                personToEdit.getPhone(),
+                personToEdit.getEmail(),
+                personToEdit.getAddress(),
+                tags
+        );
     }
 
     @Override
