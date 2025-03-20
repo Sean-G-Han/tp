c11f50245ae4cf0d0c5933896f66802b95e159aa
Nicholas Foo <foosekwoon@gmail.com>
2025-03-20 14:49:29 +0800

    Add and Refactor for FindClientCommand

---
+++ src/main/java/seedu/address/logic/commands/FindClientCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.client.NameContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all clients in address book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FindClientCommand extends Command {
+
+    public static final String COMMAND_WORD = "findclient";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all clients whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final NameContainsKeywordsPredicate predicate;
+
+    public FindClientCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredClientList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_CLIENTS_LISTED_OVERVIEW, model.getFilteredClientList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindClientCommand)) {
+            return false;
+        }
+
+        FindClientCommand otherFindClientCommand = (FindClientCommand) other;
+        return predicate.equals(otherFindClientCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
