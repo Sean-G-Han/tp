8a61e1cbc4f9385b3f7ecdd4f07db0df9bdf352d
joshua <joshua@kata.ai>
2025-03-20 14:13:55 +0800

    add update command

---
+++ src/main/java/seedu/address/logic/commands/UpdateClientCommand.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Updates the details of an existing client in the address book.
+ * This is an alias for the EditCommand.
+ */
+public class UpdateClientCommand extends EditCommand {
+
+    public static final String COMMAND_WORD = "update";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the details of the client identified "
+            + "by the index number used in the displayed client list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_PHONE + "91234567 "
+            + PREFIX_EMAIL + "johndoe@example.com";
+
+    public static final String MESSAGE_UPDATE_CLIENT_SUCCESS = "Updated Client: %1$s";
+
+    /**
+     * @param index of the client in the filtered client list to edit
+     * @param editClientDescriptor details to edit the client with
+     */
+    public UpdateClientCommand(Index index, EditClientDescriptor editClientDescriptor) {
+        super(index, editClientDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        
+        // Reuse the EditCommand execute logic but with a different success message
+        CommandResult result = super.execute(model);
+        
+        // Replace the edit success message with the update success message
+        String successMessage = String.format(
+            MESSAGE_UPDATE_CLIENT_SUCCESS, 
+            result.getFeedbackToUser().substring(
+                MESSAGE_EDIT_CLIENT_SUCCESS.indexOf("%1$s") + 4 // Length of "%1$s"
+            )
+        );
+        
+        return new CommandResult(successMessage);
+    }
+} 
