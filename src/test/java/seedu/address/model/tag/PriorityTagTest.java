99ef3a7b4262e57b7dd9c0b0a7c89868a3d121b8
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-23 16:19:10 +0800

    Add more tests

--- src/test/java/seedu/address/model/tag/PriorityTagTest.java
+++ src/test/java/seedu/address/model/tag/PriorityTagTest.java
@@ -1,10 +1,18 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class PriorityTagTest {
+
+    @Test
+    void testDefaultConstructor() {
+        // Test the default constructor
+        PriorityTag priorityTag = new PriorityTag();
+        assertEquals("Priority", priorityTag.tagName);
+    }
 
     @Test
     public void constructor_null_throwsNullPointerException() {
--- src/test/java/seedu/address/model/tag/TagFactoryTest.java
+++ src/test/java/seedu/address/model/tag/TagFactoryTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY;
@@ -12,11 +13,14 @@ public class TagFactoryTest {
     public void create_priority_tag() {
         Tag tag = TagFactory.createTag(VALID_TAG_PRIORITY);
         assertTrue(tag instanceof PriorityTag);
+        assertEquals("Priority", tag.tagName);
     }
 
     @Test
     public void create_tag() {
         Tag tag = TagFactory.createTag("abcdefghijklmnop");
         assertFalse(tag instanceof PriorityTag);
+        assertTrue(tag instanceof Tag);
+        assertEquals("abcdefghijklmnop", tag.tagName);
     }
 }
