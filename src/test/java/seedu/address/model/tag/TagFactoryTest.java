a46220fa3db7ca58b7ea09b7f38fcc6145e2798b
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-03-23 16:32:57 +0800

    Update test function

--- src/test/java/seedu/address/model/tag/TagFactoryTest.java
+++ src/test/java/seedu/address/model/tag/TagFactoryTest.java
@@ -3,15 +3,14 @@ package seedu.address.model.tag;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_PRIORITY;
 
 import org.junit.jupiter.api.Test;
 
 public class TagFactoryTest {
 
     @Test
-    public void create_priority_tag() {
-        Tag tag = TagFactory.createTag(VALID_TAG_PRIORITY);
+    public void create_priorityTag() {
+        Tag tag = TagFactory.createTag("Priority");
         assertTrue(tag instanceof PriorityTag);
         assertEquals("Priority", tag.tagName);
     }
@@ -22,5 +21,12 @@ public class TagFactoryTest {
         assertFalse(tag instanceof PriorityTag);
         assertTrue(tag instanceof Tag);
         assertEquals("abcdefghijklmnop", tag.tagName);
+    }
+
+    @Test
+    public void create_nonPriorityTag() {
+        Tag tag = TagFactory.createTag("NonPriority");
+        assertFalse(tag instanceof PriorityTag);
+        assertEquals("NonPriority", tag.tagName);
     }
 }
