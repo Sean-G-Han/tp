206eb091374b2b70f5694363ec3800de2dd30a1c
sciphi-123 <ryuni2123@gmail.com>
2025-03-27 09:36:59 +0800

    Make minor edit to sample

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,13 +28,7 @@ public class SampleDataUtil {
                     getTagSet("Policy B", "Policy C")),
             new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("Policy A")),
-            new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("Policy A")),
-            new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("Policy B")),
+                    getTagSet("Policy A"))
         };
     }
 
