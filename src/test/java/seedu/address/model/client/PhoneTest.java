bf394ad0fcb6897af9c28393ddd6479c31eef59a
curiousfun88 <curiouslearner842@gmail.com>
2025-04-03 22:01:10 +0800

    add bug corrections and refine edge case

--- src/test/java/seedu/address/model/client/PhoneTest.java
+++ src/test/java/seedu/address/model/client/PhoneTest.java
@@ -31,7 +31,6 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits (no international code)
         assertFalse(Phone.isValidPhone("42938420331212")); // more than 13 digits
         assertFalse(Phone.isValidPhone("+1234 1234567891234")); // too many digits after international code
         assertFalse(Phone.isValidPhone("+123456789")); // international code too long
